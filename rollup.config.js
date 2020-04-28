4c486ef11dddd7327d156b6a5c3dbeeb2ad0f67c
Jae Bradley <jae.b.bradley@gmail.com>
2020-04-28 18:36:25 -0400

    build(dependencies): update devDependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
