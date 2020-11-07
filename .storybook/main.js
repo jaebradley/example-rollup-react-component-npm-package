f18825c2b1ec7716a8a3169c68967b69416078f1
Jae Bradley <jae.b.bradley@gmail.com>
2020-11-07 14:13:21 -0500

    docs(storybook): upgrade to storybook 6

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,9 @@
 module.exports = {
   stories: ['../src/**/*.stories.jsx'],
   addons: [
-    '@storybook/addon-options/register',
-    '@storybook/addon-actions/register',
-    '@storybook/addon-a11y/register',
-    '@storybook/addon-notes/register',
+    {
+      name: '@storybook/addon-essentials',
+    },
   ],
 };
 
